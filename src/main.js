e42b9801a890ca140572d6bb158e24d73f947c26
Derek <18201961626@163.com>
2020-09-03 12:08:16 +0800

    text-demo-sxv-01

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.getElementById("ak").onclick = function () {
+  //根据id获取超链接,设置href属性
+  let aObj = document.getElementById("ak");
+  aObj.href = "http://www.baidu.com";
+  //根据id获取超链接,设置文字内容
+  aObj.innerText = "baidu";
+};
